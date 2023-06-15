3e9f2e81549da7e07000e728aa9d45259c7aec00
mohammed almuzayyen <49112335+almuzayyen@users.noreply.github.com>
2023-06-15 10:14:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ def main():
 @app.route('/mainPage')
 def hello():
     return 'you are in main page'
+@app.route('/read_file')
+def read_file():
+    f = open("/data//home/user")
+    contents = f.read()
+    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
